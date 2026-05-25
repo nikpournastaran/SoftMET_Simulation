bdab754a206fb553a7ababfef8d87554f21aa51e
Nastaran Nikpour <nastarannikpour7@gmail.com>
2026-05-25 08:31:07 +0200

    Update 3Tree_Scenario1.R

--- SoftMET_simulation.R/3Tree_Scenario1.R
+++ SoftMET_simulation.R/3Tree_Scenario1.R
@@ -1,441 +1,113 @@
+# load libraries
 library(lme4)
+library(MASS)
+library(knitr)
+set.seed(2026)
 
-# Softmax 
+
+
+# Softmax
 get_pi <- function(H, th) {
   sc <- as.matrix(H) %*% t(th)
-  sc <- sc - apply(sc, 1, max) # avoid overflow
+  sc <- sc - apply(sc, 1, max) # numerical stability
   exp_h <- exp(sc)
   return(exp_h / rowSums(exp_h))
 }
 
-# Update a single tree using BFGS
+# Update routing parameters using BFGS
 upd_tree <- function(Y, H, th, n_l) {
   p_ncol <- ncol(H)
-  
-  # Drop first column to fix collinearity 
   get_mu <- function(p) { 
     m <- coef(lm(Y ~ p[, -1, drop = FALSE]))
     m[is.na(m)] <- 0
     return(as.numeric(m))
   }
-  
-  # MSE loss function
   obj <- function(v) {
     p <- get_pi(H, matrix(v, nrow = n_l, ncol = p_ncol, byrow = TRUE))
     m <- get_mu(p)
     yh <- m[1] + p[, -1, drop = FALSE] %*% m[-1]
     mean((Y - yh)^2)
   }
-  
-  opt <- optim(as.vector(t(th)), obj, method = "BFGS", control = list(maxit = 50))
-  new_th <- matrix(opt$par, nrow = n_l, ncol = p_ncol, byrow = TRUE)
-  return(list(th = new_th, pi = get_pi(H, new_th)))
+  opt <- optim(as.vector(t(th)), obj, method = "BFGS", control = list(maxit = 30))
+  return(list(th = matrix(opt$par, nrow = n_l, ncol = p_ncol, byrow = TRUE)))
 }
 
-# Generate data for scenario 1
-gen_data <- function(n = 500, g = 50) {
+#  Data Generation
+
+gen_data <- function(n = 500, g = 50, scenario = 1) {
   gr <- factor(rep(1:g, each = n/g))
-  X1 <- rnorm(n); X2 <- rnorm(n)
-  Z1 <- rnorm(g)[gr]; Z2 <- rnorm(g)[gr]
-  Y <- 5 + X1 + Z1 + rnorm(g, 0, sqrt(3))[gr] + rnorm(n)
+  Sigma <- matrix(c(1.0, 0.4, 0.4, 1.0), 2, 2)
+  X <- mvrnorm(n, mu = c(0, 0), Sigma = Sigma)
+  X1 <- X[,1]; X2 <- X[,2]
+  Z_unique <- mvrnorm(g, mu = c(0, 0), Sigma = Sigma)
+  Z1 <- Z_unique[gr, 1]; Z2 <- Z_unique[gr, 2]
+  u_j <- rnorm(g, 0, sqrt(3))[gr]
+  eps <- rnorm(n, 0, 1)
+  
+  # Scenarios: 1=Linear, 2=Quasi-linear, 3=Non-linear
+  if(scenario == 1) Y <- 5 + X1 + Z1 + u_j + eps
+  if(scenario == 2) Y <- 5 + X1 + Z1 + 2*(X1>=0) + 3*(Z2<0) - 3*(X1<0 & Z2>=0) + u_j + eps
+  if(scenario == 3) Y <- 2*X1 + 4*Z1 + 2*X2^2 + 2*Z1*log(abs(X1)+0.01) + u_j + eps
+  
   return(data.frame(Y, X1, X2, Z1, Z2, gr))
 }
 
-
-# Softmet 3-Trees Backfitting Algorithm (Prof's Structure)
-softmet_3trees <- function(Y, X, gr, covLin=NULL, covT1=NULL, covT2=NULL, covT3=NULL,
-                           niter = 50, re_form = "(1|gr)", n_leaves = 4, prec=1e-4, extended=FALSE) {
-  
-  # Input validation
-  if (NROW(gr) != NROW(X)) {
-    stop("Invalid input: gr must have the same length/rows as the number of rows in X.")
-  }
-  
-  if (!grepl("^\\(.*\\)$", re_form)) {
-    re_form <- paste0("(", re_form, ")")
-  }
-  
-  # Settings
-  library(lme4)
-  
-  if(!is.null(covLin)) covLin  <- paste0(covLin , collapse="+")
-  if(is.null(covLin)) covLin  <- "1"
-  
-  mydata <- data.frame(Y, X, gr)
-  p <- ncol(X)
-  n <- nrow(X)
-  
-  # Prepare Matrix H for Softmax trees based on provided covariates
-  all_covs <- unique(c(covT1, covT2, covT3))
-  if(is.null(all_covs)) {
-    H <- as.matrix(X)
-  } else {
-    H <- as.matrix(X[, all_covs, drop=FALSE])
-  }
-  p_ncol <- ncol(H)
-  
-  myform1 <- as.formula(paste0("Y_pres ~ ", covLin, " + ", re_form))
-  
-  #initialization 
-  YhatT1 <- YhatT2 <- YhatT3 <- mean(Y)/3
-  YhatL <- predict(lm(as.formula(paste0("Y ~ ", covLin)), data=mydata))
-  
-  # Initialize 3 separate theta matrices for Softmax
-  th1 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  th2 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  th3 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  
-  msetrain <- mse.best <- 10000000
-  t <- 0 
-  d <- 1
-  
-  #### Start iterative procedure
-  while (d != 0){
-    msetrain_old <- msetrain
-    
-    ##### Compute residuals
-    Y_residuals <- Y - YhatL - YhatT1 - YhatT2 - YhatT3
-    
-    ### Linear part
-    Y_pres <- Y_residuals + YhatL
-    mydata$Y_pres <- Y_pres
-    mod1 <- suppressWarnings(lmer(myform1, REML=FALSE, data=mydata, verbose = 0))
-    YhatL <- predict(mod1, re.form = NULL, random.only=FALSE, type="response")
-    
-    ### fit tree T1
-    Y_pres <- Y_pres - YhatL + YhatT1
-    YhatT1 <- rep(0,n); tr1 <- NULL
-    if(!is.null(covT1)){
-      H1 <- as.matrix(X[, covT1, drop=FALSE])
-      tr1 <- upd_tree(Y_pres, H1, th1, n_leaves)
-      YhatT1 <- predict(lm(Y_pres ~ tr1$pi[, -1, drop = FALSE]))
-    }
-    
-    ### fit tree T2
-    Y_pres <- Y_pres - YhatT1 + YhatT2
-    YhatT2 <- rep(0,n); tr2 <- NULL
-    if(!is.null(covT2)){
-      H2 <- as.matrix(X[, covT2, drop=FALSE])
-      tr2 <- upd_tree(Y_pres, H2, th2, n_leaves)
-      YhatT2 <- predict(lm(Y_pres ~ tr2$pi[, -1, drop = FALSE]))
-    }
-    
-    ### fit tree T3
-    Y_pres <- Y_pres - YhatT2 + YhatT3
-    YhatT3 <- rep(0,n); tr3 <- NULL
-    if(!is.null(covT3)){
-      H3 <- as.matrix(X[, covT3, drop=FALSE])
-      tr3 <- upd_tree(Y_pres, H3, th3, n_leaves)
-      YhatT3 <- predict(lm(Y_pres ~ tr3$pi[, -1, drop = FALSE]))
-    }
-    
-    pred.final <- YhatL + YhatT1 + YhatT2 + YhatT3
-    msetrain <- mean((Y-pred.final)^2)
-    
-    t=t+1
-    cat(paste0(rep('=', t), collapse = ''))
-    d <- (abs(msetrain_old - msetrain)> prec)*(t < niter)
-    
-    # update best trees
-    if (msetrain < mse.best) {
-      mse.best <- msetrain
-      th1.best <- if(!is.null(tr1)) tr1$th else th1
-      th2.best <- if(!is.null(tr2)) tr2$th else th2
-      th3.best <- if(!is.null(tr3)) tr3$th else th3
-    }
-  }#### End iterative procedure
-  
-  #########################
-  # construct final model matrix
-  mydata2 <- as.data.frame(cbind(Y, X, gr))
-  
-  f_trees <- c()
-  if(!is.null(covT1)){
-    H1 <- as.matrix(X[, covT1, drop=FALSE])
-    Phi1 <- as.data.frame(get_pi(H1, th1.best)[, -1, drop = FALSE])
-    names(Phi1) <- paste0("T1_V", 1:ncol(Phi1))
-    mydata2 <- cbind(mydata2, Phi1)
-    f_trees <- c(f_trees, names(Phi1))
-  }
-  if(!is.null(covT2)){
-    H2 <- as.matrix(X[, covT2, drop=FALSE])
-    Phi2 <- as.data.frame(get_pi(H2, th2.best)[, -1, drop = FALSE])
-    names(Phi2) <- paste0("T2_V", 1:ncol(Phi2))
-    mydata2 <- cbind(mydata2, Phi2)
-    f_trees <- c(f_trees, names(Phi2))
-  }
-  if(!is.null(covT3)){
-    H3 <- as.matrix(X[, covT3, drop=FALSE])
-    Phi3 <- as.data.frame(get_pi(H3, th3.best)[, -1, drop = FALSE])
-    names(Phi3) <- paste0("T3_V", 1:ncol(Phi3))
-    mydata2 <- cbind(mydata2, Phi3)
-    f_trees <- c(f_trees, names(Phi3))
-  }
-  
-  # Formulate final formula
-  myform3 <- paste0("Y ~ ", covLin)
-  if(covLin == "1") myform3 <- "Y ~ "
-  if(length(f_trees) > 0) myform3 <- paste0(myform3, " + ", paste(f_trees, collapse = " + "))
-  myform3 <- as.formula(paste0(myform3, "+", re_form))
-  
-  cat("\n", "Estimating the final model...","\n")
-  mod.final <- suppressMessages(lmer(myform3, REML=FALSE, data=mydata2, verbose = 0))
-  
-  mse.final <- mean((Y - predict(mod.final, re.form = ~0, random.only=FALSE, type="response"))^2)
-  mse.random  <- mean((Y - predict(mod.final, re.form = NULL, random.only=FALSE, type="response"))^2)
-  
-  myris <- list(mse.final=mse.final, mse.random=mse.random, mod.final= mod.final)
-  if(!is.null(covT1)) myris$th1 <- th1.best
-  if(!is.null(covT2)) myris$th2 <- th2.best
-  if(!is.null(covT3)) myris$th3 <- th3.best
-  if(extended) myris$modmat <- mydata2
-  return(myris)
-}
-
-# simulation and test 
-sim_data <- gen_data()
-
-Y_val <- sim_data$Y
-X_mat <- sim_data[, c("X1", "X2", "Z1", "Z2")]
-gr_val <- sim_data$gr
-
-# Calling our function exactly like the prof's syntax
-res <- softmet_3trees(Y = Y_val, 
-                      X = X_mat, 
-                      gr = gr_val, 
-                      covLin = c("X1", "X2", "Z1", "Z2"),
-                      covT1 = c("X1", "X2"), 
-                      covT2 = c("Z1", "Z2"), 
-                      covT3 = c("X1", "Z2"),
-                      niter = 5)
-
-# Review final summary
-summary(res$mod.final)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-library(lme4)
-
-# Softmax 
-get_pi <- function(H, th) {
-  sc <- as.matrix(H) %*% t(th)
-  sc <- sc - apply(sc, 1, max) # avoid overflow
-  exp_h <- exp(sc)
-  return(exp_h / rowSums(exp_h))
-}
-
-# Update a single tree using BFGS
-upd_tree <- function(Y, H, th, n_l) {
-  p_ncol <- ncol(H)
-  
-  # Drop first column to fix collinearity 
-  get_mu <- function(p) { 
-    m <- coef(lm(Y ~ p[, -1, drop = FALSE]))
-    m[is.na(m)] <- 0
-    return(as.numeric(m))
-  }
-  
-  # MSE loss function
-  obj <- function(v) {
-    p <- get_pi(H, matrix(v, nrow = n_l, ncol = p_ncol, byrow = TRUE))
-    m <- get_mu(p)
-    yh <- m[1] + p[, -1, drop = FALSE] %*% m[-1]
-    mean((Y - yh)^2)
-  }
-  
-  opt <- optim(as.vector(t(th)), obj, method = "BFGS", control = list(maxit = 50))
-  new_th <- matrix(opt$par, nrow = n_l, ncol = p_ncol, byrow = TRUE)
-  return(list(th = new_th, pi = get_pi(H, new_th)))
-}
-
-# Generate data for scenario 1
-gen_data <- function(n = 500, g = 50) {
-  gr <- factor(rep(1:g, each = n/g))
-  X1 <- rnorm(n); X2 <- rnorm(n)
-  Z1 <- rnorm(g)[gr]; Z2 <- rnorm(g)[gr]
-  Y <- 5 + X1 + Z1 + rnorm(g, 0, sqrt(3))[gr] + rnorm(n)
-  return(data.frame(Y, X1, X2, Z1, Z2, gr))
-}
-
-# 3-tree backfitting and final inference
+# SoftMET algorithm with 3-tree backfitting
 softmet_3trees <- function(d, n_leaves = 4, niter = 5) {
-  H <- as.matrix(d[, c("X1", "X2", "Z1", "Z2")])
-  p_ncol <- ncol(H)
+  X_vars <- as.matrix(d[, c("X1", "X2", "Z1", "Z2")])
+  p_ncol <- ncol(X_vars)
+  th1 <- th2 <- th3 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
+  yhT1 <- yhT2 <- yhT3 <- mean(d$Y)/3
   
-  # Initialize 3 separate theta matrices
-  th1 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  th2 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  th3 <- matrix(runif(n_leaves * p_ncol, -0.1, 0.1), nrow = n_leaves)
-  
-  # Initial tree predictions divided by 3 like the prof's code
-  yhT1 <- yhT2 <- yhT3 <- mean(d$Y) / 3
-  
-  # Stage 1: Backfitting loop 
   for (i in 1:niter) {
-    # Update linear part via lmer
-    d$Y_resid <- d$Y - (yhT1 + yhT2 + yhT3)
-    yhL <- fitted(lmer(Y_resid ~ X1 + X2 + Z1 + Z2 + (1|gr), data = d, REML = FALSE))
+    # Update linear part
+    d$Y_res <- d$Y - (yhT1 + yhT2 + yhT3)
+    mod_l <- lmer(Y_res ~ X1 + X2 + Z1 + Z2 + (1|gr), data = d, REML = FALSE)
+    yhL <- fitted(mod_l)
     
-    # Update Tree 1
-    Y_p1 <- d$Y - yhL - yhT2 - yhT3
-    tr1 <- upd_tree(Y_p1, H, th1, n_leaves)
-    th1 <- tr1$th
-    yhT1 <- predict(lm(Y_p1 ~ tr1$pi[, -1, drop = FALSE]))
+    # Backfitting tree components
+    tr1 <- upd_tree(d$Y-yhL-yhT2-yhT3, X_vars, th1, n_leaves); th1 <- tr1$th
+    yhT1 <- predict(lm((d$Y-yhL-yhT2-yhT3) ~ get_pi(X_vars, th1)[,-1]))
     
-    # Update Tree 2
-    Y_p2 <- d$Y - yhL - yhT1 - yhT3
-    tr2 <- upd_tree(Y_p2, H, th2, n_leaves)
-    th2 <- tr2$th
-    yhT2 <- predict(lm(Y_p2 ~ tr2$pi[, -1, drop = FALSE]))
+    tr2 <- upd_tree(d$Y-yhL-yhT1-yhT3, X_vars, th2, n_leaves); th2 <- tr2$th
+    yhT2 <- predict(lm((d$Y-yhL-yhT1-yhT3) ~ get_pi(X_vars, th2)[,-1]))
     
-    # Update Tree 3
-    Y_p3 <- d$Y - yhL - yhT1 - yhT2
-    tr3 <- upd_tree(Y_p3, H, th3, n_leaves)
-    th3 <- tr3$th
-    yhT3 <- predict(lm(Y_p3 ~ tr3$pi[, -1, drop = FALSE]))
+    tr3 <- upd_tree(d$Y-yhL-yhT1-yhT2, X_vars, th3, n_leaves); th3 <- tr3$th
+    yhT3 <- predict(lm((d$Y-yhL-yhT1-yhT2) ~ get_pi(X_vars, th3)[,-1]))
   }
   
-  #  Stage 2: Final inference (Algorithm 1)
-  # Get bases and drop first column for all 3 trees
-  Phi1 <- as.data.frame(get_pi(H, th1)[, -1, drop = FALSE])
-  Phi2 <- as.data.frame(get_pi(H, th2)[, -1, drop = FALSE])
-  Phi3 <- as.data.frame(get_pi(H, th3)[, -1, drop = FALSE])
+  # Stage 2: Final Inference using soft-leaf basis
+  Phi1 <- get_pi(X_vars, th1)[,-1]; Phi2 <- get_pi(X_vars, th2)[,-1]; Phi3 <- get_pi(X_vars, th3)[,-1]
+  df_fin <- cbind(d[,1:6], Phi1, Phi2, Phi3)
+  colnames(df_fin)[7:ncol(df_fin)] <- paste0("Basis_", 1:(3*(n_leaves-1)))
   
-  names(Phi1) <- paste0("T1_V", 1:ncol(Phi1))
-  names(Phi2) <- paste0("T2_V", 1:ncol(Phi2))
-  names(Phi3) <- paste0("T3_V", 1:ncol(Phi3))
-  
-  # Combine data (augmented design matrix)
-  Phi_all <- cbind(Phi1, Phi2, Phi3)
-  D_final <- cbind(d[, c("Y", "X1", "X2", "Z1", "Z2", "gr")], Phi_all)
-  
-  # Fit base model and final softmet model
-  m_base <- lmer(Y ~ X1 + X2 + Z1 + Z2 + (1|gr), data = D_final, REML = FALSE)
-  
-  f_trees <- paste(names(Phi_all), collapse = " + ")
-  form_soft <- as.formula(paste("Y ~ X1 + X2 + Z1 + Z2 +", f_trees, "+ (1|gr)"))
-  m_soft <- lmer(form_soft, data = D_final, REML = FALSE)
+  m_base <- lmer(Y ~ X1 + X2 + Z1 + Z2 + (1|gr), data = df_fin, REML = FALSE)
+  f_soft <- as.formula(paste("Y ~ X1+X2+Z1+Z2 +", paste(colnames(df_fin)[7:ncol(df_fin)], collapse="+"), "+ (1|gr)"))
+  m_soft <- lmer(f_soft, data = df_fin, REML = FALSE)
   
   return(list(base = m_base, soft = m_soft))
 }
 
-#  Run simulation and test 
-my_data <- gen_data()
-res <- softmet_3trees(my_data)
 
-# Compare models using the prof's anova step
-anova(res$base, res$soft)
+
+# 1. Single Run ANOVA
+cat("\n--- Single Run ANOVA Comparison ---\n")
+results_anova <- lapply(1:3, function(sc) {
+  fit <- softmet_3trees(gen_data(scenario = sc))
+  res <- anova(fit$base, fit$soft)
+  data.frame(Scenario=sc, AIC_Base=res$AIC[1], AIC_Soft=res$AIC[2], P_Val=format.pval(res$`Pr(>Chisq)`[2], eps=0.001))
+})
+print(kable(do.call(rbind, results_anova), digits=2))
+
+# 2. Monte Carlo Simulation
+cat("\n--- Monte Carlo Simulation (10 Iterations) ---\n")
+mc_bench <- lapply(1:3, function(sc) {
+  mse_b <- mse_s <- sig <- numeric(10)
+  for(i in 1:10) {
+    f <- softmet_3trees(gen_data(scenario = sc))
+    mse_b[i] <- mean(residuals(f$base)^2); mse_s[i] <- mean(residuals(f$soft)^2)
+    sig[i] <- anova(f$base, f$soft)$`Pr(>Chisq)`[2] < 0.05
+  }
+  data.frame(Scenario=sc, Power=mean(sig), MSE_Base=mean(mse_b), MSE_Soft=mean(mse_s), Imp=mean(mse_b-mse_s))
+})
+print(kable(do.call(rbind, mc_bench), digits=3))
